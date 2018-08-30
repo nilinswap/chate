53db52b435f16ec8263c6aa689cf83b369652ac5
nilinswap <nilinswap@gmail.com>
2018-08-30 13:14:16 +0530

    made ports a static member for server.

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 #server.py
 import socket
 class Server:
+	port = 7777
+	cli_service_port = 8888
 	def __init__(self, ip = ''):
 		'''
 			stores name to ip map in dic.
 		'''
-		self.adr = ip
-		self.port = 7777
+		self.ip = ip
 		self.map = {}
 		self.port_pool = set([])
-		self.cli_listen_port = 8888
+		
 
 	def run(self):
 		'''
@@ -17,27 +18,27 @@ class Server:
 			It also registers a name with IP. so message can be of two types. register or reply.
 		'''
 		self.sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM)
-		self.sock.bind(('', self.port))
+		self.sock.bind(('', self.port)) #binds this process('s socket) to the port. See Ip binding is not possible because are you faking??.
 
 		while True:
 			msg, addr = self.sock.recvfrom(1024)
 			msg = msg.decode()
 			lis = msg.split(' ')
-			if lis[0] == 'register':
+			if lis[0] == 'register': # e.g. register new_name
 				name = lis[1]
 				ip = addr[0]
 				#this is the part where I make entry
 				self.map[name] = ip
 				print(self.map)
 				# INCREASE RELIABLITY HERE
-			elif lis[0] == 'close':
+			elif lis[0] == 'close':  # e.g. close name
 				name = lis[1]
 				ip = addr[0]
 				#this is the part where I remove entry
 				del(self.map[name])
 				print(self.map)
 				# INCREASE RELIABLITY HERE		
-			elif lis[0] == 'request':
+			elif lis[0] == 'request': # e.g. request name
 				name = lis[1]
 				try:
 					ip = self.map[name]
@@ -47,7 +48,7 @@ class Server:
 					new_msg = 'error ' + e
 				finally:
 					new_msg = new_msg.encode();
-					adr_tup = addr[0], self.cli_listen_port
+					adr_tup = addr[0], self.cli_service_port
 					self.sock.sendto(new_msg, adr_tup)
 			else:
 				print("else")
